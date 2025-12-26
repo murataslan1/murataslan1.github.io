0c76b0416f80b707d35b3a92ba28a15ef93a1aa5
Murat Aslan <murataslan1@users.noreply.github.com>
2025-12-26 14:10:15 +0300

    Redesign portfolio with Retro-Futuristic Terminal aesthetic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+// React 18 createRoot API
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
